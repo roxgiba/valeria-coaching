e1e96e91909dea0a2b7383d8d8aba2e551bc7f08
roxgiba <rox.giba@gmail.com>
2023-09-10 16:59:21 +0200

    main title modified

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className=" object-none object-bottom min-h-screen bg-top bg-no-repeat bg-[url(https://i.etsystatic.com/37733314/r/il/a91c4b/4234176747/il_1140xN.4234176747_3vkp.jpg)]">
         <div className="flex space-x-80 bg-gradient-to-r from-slate-200/50 via-slate-500/50 to-slate-200/50 h-24  ">
           <h1 className="text-black font-sans text-5xl py-5 px-36 tracking-widest inline-block">
-            Coaching by Valeria Topai
+            Coaching con Valeria Topai
           </h1>
           <Dropdown />
         </div>
