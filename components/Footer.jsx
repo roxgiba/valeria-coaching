c9ae4c55813f3df6382dd407a1a84fdd8b8182f5
roxgiba <rox.giba@gmail.com>
2023-09-13 15:35:43 +0200

    footer icons linked

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,47 +1,55 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white">
-      {/* <div>
-        <Image
-          src="/logo-coaching.jpeg"
-          alt="logo escuela europea de coaching"
-          width={50}
-          height={50}
-          className=""
-        />
-        <Image
-          src="/logo-coaching2.jpeg"
-          alt="logo international coaching federation"
-          width={50}
-          height={50}
-          className=""
-        />
-      </div> */}
-      <div className="md:flex md:justify-evenly md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+      <div className="md:flex md:justify-evenly md:items-center sm:px-12 px-4 bg-slate-700 py-7">
+        <div className="flex justify-start space-x-4 me-3 pb-8 lg:pb-5 ">
+          <Link href="https://www.linkedin.com/in/valeria-topai-poveda-3b2b7697/">
+            <Image
+              src="/logo-linkedin.png"
+              alt="logo Linkedin"
+              width={30}
+              height={30}
+              className="rounded-full"
+            />
+          </Link>
+          <Link href="https://www.escuelacoaching.com/">
+            <Image
+              src="/logo-coaching.jpeg"
+              alt="logo escuela europea de coaching"
+              width={30}
+              height={30}
+              className="rounded-full"
+            />
+          </Link>
+
+          <Link href="https://coachingfederation.org/">
+            <Image
+              src="/logo-coaching2.jpeg"
+              alt="logo international coaching federation"
+              width={30}
+              height={30}
+              className="rounded-full"
+            />
+          </Link>
+        </div>
         <h1
-          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+          className="p-2 lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold text-center
      md:w-2/5"
         >
           ¡Hablemos!
         </h1>
-        <div>
-          <button
-            className="bg-rose-400 hover:bg-blue-500 duration-300 px-5 py-2.5 font-[Poppins]
-       rounded-md text-white md:w-auto w-full"
-          >
-            Reserva tu primera sesión gratuita
-          </button>
-        </div>
+        <button
+          className="bg-white hover:bg-blue-500 duration-300 px-5 py-2.5 font-[Poppins]
+       rounded-md text-black hover:text-white md:w-auto w-full"
+        >
+          Reserva tu primera sesión gratuita
+        </button>
       </div>
-      <div
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
-  text-center pt-2 text-gray-400 text-sm pb-8"
-      >
-        <span> </span>
-
+      <div className="justify-center text-center pt-3 text-gray-400 text-sm pb-8">
         <span>© 2023 Valeria Topai. All rights reserved.</span>
       </div>
     </footer>
@@ -49,3 +57,11 @@ function Footer() {
 }
 
 export default Footer;
+
+/*
+
+
+
+
+
+*/
