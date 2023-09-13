b96689f64da3a59fb32e1633fb43b247072b3ef1
roxgiba <rox.giba@gmail.com>
2023-09-13 13:14:42 +0200

    boxes in coaching created

--- components/Coaching.jsx
+++ components/Coaching.jsx
@@ -2,28 +2,33 @@ import React from "react";
 
 function Coaching() {
   return (
-    <div>
-      <h1>Coaching</h1>
-      <div>
-        <p>
+    <div className="">
+      <div className=" tracking-wide font-sans pt-32 sm:pt-36 lg:pt-6 text-justify grid justify-items-stretch">
+        <h1 className="font-bold text-amber-700 text-2xl  lg:pt-20 pb-20 justify-self-center italic">
+          Coaching
+        </h1>
+      </div>
+
+      <div className="px-72 text-white text-center text-xl  grid justify-items-center">
+        <div className="bg-rose-300	   rounded-md place-self-start box-border w-96 p-10 border-4">
           Se trata de un proceso de autodescubrimiento que te ayudará a conectar
           con tus objetivos y tu compromiso para alcanzarlos.
-        </p>
-        <p>
+        </div>
+        <div className="bg-rose-300	 box-border w-96 p-10 border-4 rounded-md place-self-end">
           El proceso consiste en varias sesiones de una hora aproximadamente
           cada una, (normalmente un mínimo de 5) en las cuales el coach te va a
           acompañar, con preguntas, feedbacks y en ocasiones ejercicios, hacia
           ese lugar donde quieres estar.
-        </p>
-        <p>
+        </div>
+        <div className="bg-rose-300	 box-border w-96 p-10 border-4 rounded-md place-self-start">
           Sea cual sea tu meta, un proceso de Coaching te ayudará a descubrir
           que necesitas incorporar y que necesitas soltar para conseguir lo que
           quieres.{" "}
-        </p>
-        <p>
+        </div>
+        <div className="bg-rose-300	 box-border w-96 p-10 border-4 rounded-md place-self-end">
           Mi misión como coach es acompañarte en este proceso para que te
           conviertas en tu mejor maestro para alcanzar tu propio éxito!
-        </p>
+        </div>
       </div>
     </div>
   );
