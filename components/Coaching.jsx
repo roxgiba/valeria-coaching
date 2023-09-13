ebf9b6ac3b0522d7a3e000ce8360e7eee1f2cb21
roxgiba <rox.giba@gmail.com>
2023-09-13 12:33:04 +0200

    coaching component added to page

--- components/Coaching.jsx
+++ components/Coaching.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function Coaching() {
-  return <div>Coaching</div>;
+  return (
+    <div>
+      <h1>Coaching</h1>
+      <div>
+        <p>
+          Se trata de un proceso de autodescubrimiento que te ayudará a conectar
+          con tus objetivos y tu compromiso para alcanzarlos.
+        </p>
+        <p>
+          El proceso consiste en varias sesiones de una hora aproximadamente
+          cada una, (normalmente un mínimo de 5) en las cuales el coach te va a
+          acompañar, con preguntas, feedbacks y en ocasiones ejercicios, hacia
+          ese lugar donde quieres estar.
+        </p>
+        <p>
+          Sea cual sea tu meta, un proceso de Coaching te ayudará a descubrir
+          que necesitas incorporar y que necesitas soltar para conseguir lo que
+          quieres.{" "}
+        </p>
+        <p>
+          Mi misión como coach es acompañarte en este proceso para que te
+          conviertas en tu mejor maestro para alcanzar tu propio éxito!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Coaching;
