5cdde6b1060a6c36d73ccf17a0d719ec9eece222
roxgiba <rox.giba@gmail.com>
2023-12-06 14:57:37 +0100

    coaching box style updated

--- components/Coaching.jsx
+++ components/Coaching.jsx
@@ -9,23 +9,23 @@ function Coaching() {
         </h1>
       </div>
 
-      <div className=" lg:px-72 text-white text-center text-lg lg:text-xl  grid justify-items-center">
-        <div className="bg-rose-300	   rounded-md place-self-start box-border w-64 lg:w-96 p-10 m-5 border-4 shadow-2xl border-double">
+      <div className=" lg:px-72  text-black text-center text-lg lg:text-2xl  grid justify-items-center">
+        <div className=" bg-red-100 rounded-md place-self-start box-border w-64 lg:w-96 p-10 m-5 border-4 shadow-2xl border-double">
           Se trata de un proceso de autodescubrimiento que te ayudará a conectar
           con tus objetivos y tu compromiso para alcanzarlos.
         </div>
-        <div className="bg-rose-300	 box-border w-72 lg:w-96 p-10 m-5 border-4 rounded-md place-self-end shadow-2xl border-double ">
+        <div className="bg-red-100 box-border w-72 lg:w-96 p-10 m-5 border-4 rounded-md place-self-end shadow-2xl border-double ">
           El proceso consiste en varias sesiones de una hora aproximadamente
           cada una, (normalmente un mínimo de 5) en las cuales el coach te va a
           acompañar, con preguntas, feedbacks y en ocasiones ejercicios, hacia
           ese lugar donde quieres estar.
         </div>
-        <div className="bg-rose-300	 box-border w-64 lg:w-96 p-10 m-5 border-4 rounded-md place-self-start shadow-2xl border-double ">
+        <div className="bg-red-100 box-border w-64 lg:w-96 p-10 m-5 border-4 rounded-md place-self-start shadow-2xl border-double ">
           Sea cual sea tu meta, un proceso de Coaching te ayudará a descubrir
           que necesitas incorporar y que necesitas soltar para conseguir lo que
           quieres.{" "}
         </div>
-        <div className="bg-rose-300	 box-border w-60 lg:w-96 p-10 border-4  m-5 rounded-md place-self-end shadow-2xl border-double">
+        <div className="bg-red-100 box-border w-60 lg:w-96 p-10 border-4  m-5 rounded-md place-self-end shadow-2xl border-double">
           Mi misión como coach es acompañarte en este proceso para que te
           conviertas en tu mejor maestro para alcanzar tu propio éxito!
         </div>
