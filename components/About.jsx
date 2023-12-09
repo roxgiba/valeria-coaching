db3e1fbef9a96090cd6eaf182293fd7d6c810b71
roxgiba <rox.giba@gmail.com>
2023-12-09 11:31:24 +0100

    padding image and text about updated

--- components/About.jsx
+++ components/About.jsx
@@ -3,37 +3,57 @@ import React from "react";
 function About() {
   return (
     <div id="about" className="text-justify grid justify-items-stretch mb-36">
-      <h3 className="font-semibold text-gray-700 text-2xl lg:text-4xl pt-10 pb-8 justify-self-center">
+      <h3 className="font-semibold text-gray-700 text-2xl lg:text-4xl mt-8 lg:py-16 justify-self-center">
         Sobre mí
       </h3>
-      <div className="px-8 text-center text-gray-800 text-lg lg:px-52 tracking-wide ">
+      <div className=" text-center text-gray-800 text-lg lg:px-52 tracking-wide ">
         <div className="leading-loose">
           <p>
-            Hola, me llamo Valeria y soy Coach especializada en desarrollo
-            personal y gestión emocional. Mi propósito es guiarte hacia el éxito
-            personal, proporcionándote las herramientas necesarias para alcanzar
-            tus metas.
+            Trabajo como Coach especializada en desarrollo personal y gestión
+            emocional.
           </p>
-
           <p>
-            Anteriormente, me dediqué al mundo de la restauración, liderando
-            equipos para comunicar el esfuerzo detrás de cada plato y botella de
-            vino. Tras un golpe emocional, descubrí el coaching como una
-            poderosa herramienta para conectar con mi esencia. Este proceso
-            transformó mi vida, alineando mis valores y revelando mi verdadera
-            vocación. <br /> A lo largo de mi vida, he superado momentos de
-            incertidumbre y desmotivación gracias al coaching. Con experiencia
-            en diversas culturas, he vivido en ciudades como Bogotá, Roma,
-            Madrid, Andorra y Barcelona.
+            Mi propósito, es acompañar a las personas a encontrar el camino
+            hacia el propio éxito.
           </p>
-
           <p>
-            Hoy, como Coach, mi objetivo es ser tu guía para que te conviertas
-            en tu mejor mentor y logres la felicidad. <br />{" "}
-            <span className="font-semibold text-xl">
-              ¡Estoy aquí para apoyarte en tu desarrollo personal y crecimiento
-              emocional!
-            </span>
+            A lo largo de mi vida, yo también he experimentado momentos de
+            incertidumbre, estancamiento y desmotivación, viviendo la sensación
+            de no tener rumbo.
+          </p>
+          <p>
+            Gracias al coaching he podido superarlos y a día de hoy vivo con
+            consciencia cada paso que doy.{" "}
+          </p>
+          <p>
+            He crecido en diferentes culturas y he tenido el privilegio de vivir
+            en Bogotá, Roma, Madrid, Andorra y ahora Barcelona.
+          </p>
+          <p>
+            Dediqué una gran parte de mi vida al mundo de la restauración,
+            desempeñando roles de maitre y sommelier en restaurantes de gran
+            prestigio. Mi principal objetivo era capacitar a mis equipos para
+            comunicar el esfuerzo que hay detrás de cada plato y cada botella de
+            vino. Tras años liderando equipos, comprendí la importancia del
+            bienestar individual: cuanto mejor te llevas contigo mismo, mejores
+            relaciones estableces con el mundo que te rodea.
+          </p>
+          <p>
+            Después de un golpe emocional significativo a nivel personal, me di
+            cuenta que algo faltaba en mi vida. Decidí detenerme y cuestionarme
+            si realmente era feliz con lo que hacía. La respuesta fue un rotundo
+            NO`. Ese momento de incertidumbre me llevó a descubrir el mundo del
+            coaching, una herramienta poderosa y realmente eficaz para conectar
+            con tu esencia y trazar tu propio camino.
+          </p>
+          <p>
+            Inicié un proceso de autodescubrimiento que transformó por completo
+            mi vida, me conectó con mi verdadera vocación y me ayudó a alinear
+            mis valores.
+          </p>
+          <p className="font-semibold">
+            Hoy, como coach, mi objetivo es acompañarte para que te conviertas
+            en tu mejor maestro y consigas lo que te hace feliz!
           </p>
         </div>
       </div>
