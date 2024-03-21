98eecdea0b930db731314b514633d0d9dc16af0f
roxgiba <rox.giba@gmail.com>
2024-03-21 21:25:17 +0100

    create contact form

--- app/contactForm/page.jsx
+++ app/contactForm/page.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { useState } from "react";
 import emailjs from "@emailjs/browser";
+import Footer from "@/components/Footer";
 
 const ContactForm = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -39,16 +40,79 @@ const ContactForm = () => {
     e.target.reset();
   };
   return (
-    <form onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" disabled={isSubmitting} />
-      {stateMessage && <p>{stateMessage}</p>}
-    </form>
+    <div>
+      <form onSubmit={sendEmail} className="sfondo min-h-screen">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 p-10">
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="user_name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Nombre
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="user_name"
+                  id="user_name"
+                  autoComplete="user_name"
+                  className="block flex-1 border-0 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="Tu nombre"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="user_name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              E-mail
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="email"
+                  name="user_email"
+                  placeholder="Tu correo electronico"
+                  className="block flex-1 border-0 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-full">
+            <label
+              htmlFor="about"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Mensaje
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="message"
+                name="message"
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+                placeholder="Escribe aqui tu pregunta..."
+              />
+            </div>
+            <p className="mt-3 text-sm leading-6 text-gray-600"></p>
+            <input
+              type="submit"
+              value="Envia"
+              disabled={isSubmitting}
+              className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            />
+            {stateMessage && <p>{stateMessage}</p>}
+          </div>
+        </div>
+      </form>
+      <Footer />
+    </div>
   );
 };
 
