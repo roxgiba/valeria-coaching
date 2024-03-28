2e42c112c0ef60e864dcdf41a8b605d0d70d37dc
roxgiba <rox.giba@gmail.com>
2024-03-28 11:59:01 +0100

    emailjs is working

--- app/contactForm/page.jsx
+++ app/contactForm/page.jsx
@@ -1,93 +1,44 @@
 "use client";
 
 import React from "react";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import emailjs from "@emailjs/browser";
 import Footer from "@/components/Footer";
 import Image from "next/image";
 import Link from "next/link";
 
 const ContactForm = () => {
-  // const [isSubmitting, setIsSubmitting] = useState(false);
-  //  const [stateMessage, setStateMessage] = useState(null);
-
-  // const service_id = process.env.YOUR_SERVICE_ID;
-  // const template_id = process.env.YOUR_TEMPLATE_ID;
-  // const public_id = process.env.YOUR_PUBLIC_KEY;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [stateMessage, setStateMessage] = useState(null);
 
   const service_id = "service_xtqinmh";
   const template_id = "template_escdw2i";
   const public_id = "3eEXggHjwJT-Xu0KE";
 
-  const form = useRef();
-
   const sendEmail = (e) => {
+    e.persist();
     e.preventDefault();
+    setIsSubmitting(true);
+    emailjs.sendForm(service_id, template_id, e.target, public_id).then(
+      (result) => {
+        setStateMessage("Message sent!");
+        setIsSubmitting(false);
+        setTimeout(() => {
+          setStateMessage(null);
+        }, 5000); // hide message after 5 seconds
+      },
+      (error) => {
+        setStateMessage("Something went wrong, please try again later");
+        setIsSubmitting(false);
+        setTimeout(() => {
+          setStateMessage(null);
+        }, 5000); // hide message after 5 seconds
+      }
+    );
 
-    emailjs
-      .sendForm(service_id, template_id, form.current, {
-        publicKey: public_id,
-      })
-      .then(
-        () => {
-          console.log("SUCCESS!");
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
+    // Clears the form after sending the email
     e.target.reset();
   };
-
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
-
-  //   emailjs
-  //     .sendForm(service_id, template_id, form.current, {
-  //       publicKey: public_id,
-  //     })
-  //     .then(
-  //       () => {
-  //         console.log("SUCCESS!");
-  //       },
-  //       (error) => {
-  //         console.log("FAILED...", error.text);
-  //       }
-  //     );
-  //   e.target.reset();
-  // };
-
-  // const sendEmail = (e) => {
-  //   e.persist();
-  //   e.preventDefault();
-  //   setIsSubmitting(true);
-  //   emailjs
-  //     .sendForm(
-  //       process.env.YOUR_SERVICE_ID,
-  //       process.env.YOUR_TEMPLATE_ID,
-  //       e.target,
-  //       process.env.YOUR_PUBLIC_KEY
-  //     )
-  //     .then(
-  //       (result) => {
-  //         setStateMessage("Message sent!");
-  //         setIsSubmitting(false);
-  //         setTimeout(() => {
-  //           setStateMessage(null);
-  //         }, 5000); // hide message after 5 seconds
-  //       },
-  //       (error) => {
-  //         setStateMessage("Something went wrong, please try again later");
-  //         setIsSubmitting(false);
-  //         setTimeout(() => {
-  //           setStateMessage(null);
-  //         }, 5000); // hide message after 5 seconds
-  //       }
-  //     );
-
-  //   // Clears the form after sending the email
-  //   e.target.reset();
-  // };
 
   return (
     <div className="sfondo min-h-screen">
@@ -165,10 +116,10 @@ const ContactForm = () => {
             <input
               type="submit"
               value="Envia"
-              //    disabled={isSubmitting}
+              disabled={isSubmitting}
               className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             />
-            {/* {stateMessage && <p>{stateMessage}</p>} */}
+            {stateMessage && <p>{stateMessage}</p>}
           </div>
         </div>
       </form>
